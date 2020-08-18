4ea458ea64fce64184b629b0a3167097bc05762b
Nadav Ivgi <nadav@shesek.info>
2020-08-18 19:37:10 +0300

    Implement RpcApi::get_net_totals()

--- client/src/client.rs
+++ client/src/client.rs
@@ -957,6 +957,12 @@ pub trait RpcApi: Sized {
             "rescanblockchain", handle_defaults(&mut args, &[0.into(), null()]))?;
         Ok((res.start_height, res.stop_height))
     }
+
+    /// Returns information about network traffic, including bytes in, bytes out,
+    /// and current time.
+    fn get_net_totals(&self) -> Result<json::GetNetTotalsResult> {
+        self.call("getnettotals", &[])
+    }
 }
 
 /// Client implements a JSON-RPC client for the Bitcoin Core daemon or compatible APIs.
